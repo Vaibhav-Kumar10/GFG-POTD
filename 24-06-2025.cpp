0d397c6fdfc057aad429f9f78e80569db557bdce
Vaibhav Kumar <144508614+Vaibhav-Kumar10@users.noreply.github.com>
2025-06-24 18:24:36 +0530

    Create 24-06-2025.cpp

---
+++ 24-06-2025.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    string maxSubseq(string& s, int k) {
+        int n = s.length(), toRemove = k; 
+        string res = "";
+        for (int i = 0; i < n; i++) {
+            while (!res.empty() && toRemove > 0 && res.back() < s[i]) {
+                res.pop_back();
+                toRemove--;
+            }
+            res.push_back(s[i]);
+        }
+        res.resize(n - k);
+        return res;
+    }
+};
